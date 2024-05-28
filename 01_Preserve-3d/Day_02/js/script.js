16cead411f90cb3d0fdd7d71083274fca2057bc8
Luis Hanco <86266962+LuisHanco@users.noreply.github.com>
2024-05-28 15:37:53 -0500

    @LuisHanco

--- 01_Preserve-3d/Day_02/js/script.js
+++ 01_Preserve-3d/Day_02/js/script.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.Onclick =() =>{
+menu.onclick =() =>{
     menu.classList.toggle('bx-x');
-    navlist.classList.toggle('.open')
+    navlist.classList.toggle('open');
 }
